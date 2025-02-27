b8c5c0412fddb252915e533eeeb81e63acef4d54
0 <0>
2025-02-27 13:57:12 +0700

    refactor(Contacts): created classes Contact and ContactSerializer

---
+++ Contacts/View/Model/Contact.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View.Model
+{
+    public class Contact
+    {
+        private string _name;
+        public string Name 
+        { 
+            get { return _name; } 
+            set
+            {
+                if (value.Length <= 50 )
+                {
+                    _name = value;
+                }
+            }
+        }
+        private string _email;
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                if (new System.ComponentModel.DataAnnotations.EmailAddressAttribute().IsValid(value))
+                {
+                    _email = value;
+                }
+            }
+        }
+        private string _phoneNumber;
+        public string PhoneNumber
+        {
+            get { return _phoneNumber; }
+            set
+            {
+                
+            }
+        }
+        public Contact() { }
+    }
+}
