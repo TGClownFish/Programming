bb8dba9de2155873faf88f548b51dbf179d3b4e6
0 <0>
2025-03-06 11:27:13 +0700

    feature(View.Model.Services): added serializathon

--- Contacts/View/Model/Services/ContactSerializer.cs
+++ Contacts/View/Model/Services/ContactSerializer.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace View.Model.Services
 {
     public static class ContactSerializer
     {
+        private static string _path = Environment.GetFolderPath(Environment.SpecialFolder.
+            MyDocuments) + "\\Contacts\\contacts.json";
+        public static void Serialize(Contact contact)
+        {
+            using (FileStream fileStream = new FileStream(_path, FileMode.OpenOrCreate))
+            {
+                JsonSerializer.SerializeAsync<Contact>(fileStream, contact);
+            }
+        }
+        public static Contact Deserialize(string path)
+        {
+            using (FileStream fileStream = new FileStream(_path, FileMode.OpenOrCreate))
+            {
+               return JsonSerializer.Deserialize<Contact>(fileStream);
+            }
+        }
     }
 }
