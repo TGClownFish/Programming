13a5a66fc16cc0afcac523beddbd49dc709a60f9
0 <0>
2025-03-27 19:20:53 +0700

    feature(Contacts): added class BoolToVisibilityConverter

---
+++ src/Contacts/View/ModelView/BoolToVisibilityConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace View.ModelView
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parametr, CultureInfo culture)
+        {
+            if ((bool)value == true)
+                return Visibility.Visible;
+            else
+                return Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
