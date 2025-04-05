c38e4ca10fa0113632d3b89050124aafbb32c000
0 <0>
2025-04-05 17:46:45 +0700

    refactor(Contact): moved part of ui into user control

---
+++ src/Contacts/View/Controls/ContactInfo.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace View.Controls
+{
+    /// <summary>
+    /// Interaction logic for ContactInfo.xaml
+    /// </summary>
+    public partial class ContactInfo : UserControl
+    {
+        public ContactInfo()
+        {
+            InitializeComponent();
+        }
+    }
+}
